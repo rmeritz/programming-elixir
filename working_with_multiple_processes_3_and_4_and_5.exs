baf970b519a89638a4a5c9f53a49cd551198af2f
Rebecca Meritz <rebecca@meritz.com>
2017-07-05 16:53:44 -0700

    Add exercise 4 and 5

--- working_with_multiple_processes_3_and_4_and_5.exs
+++ working_with_multiple_processes_3_and_4_and_5.exs
@@ -4,10 +4,12 @@ defmodule Link do
   def sad_function(parent) do
     send(parent, "I'm so sad :(")
     exit(:boom)
+    #4/0
   end
 
   def run do
-    spawn_link(Link, :sad_function, [self()])
+    #spawn_link(Link, :sad_function, [self()])
+    spawn_monitor(Link, :sad_function, [self()])
     sleep(500)
     receive do
       msg ->
